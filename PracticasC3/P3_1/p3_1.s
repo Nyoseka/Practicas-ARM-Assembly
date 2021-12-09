d7053af12856a5a6716673917264b258fb252daa
Grecia Cortes <47579115+Nyoseka@users.noreply.github.com>
2021-12-09 11:30:20 -0800

    Create p3_1.s

---
+++ PracticasC3/P3_1/p3_1.s
@@ -0,0 +1,27 @@
+.data
+seed : .word 1
+const1 : .word 1103515245
+const2 : .word 12345
+
+.text
+.global myrand, mysrand
+
+myrand : ldr r1, = seed 
+ldr r0, [r1] 
+ldr r2, [r1, #4]
+mul r3, r0, r2
+ldr r0, [r1, #8] 
+add r0, r0, r3
+str r0, [r1]
+
+
+LSL r0, # 1
+LSR r0, # 17
+bx lr
+mysrand : ldr r1, = seed
+str r0, [ r1 ]
+bx lr
+
+mysrand : ldr r1, = seed
+str r0, [ r1 ]
+bx lr
