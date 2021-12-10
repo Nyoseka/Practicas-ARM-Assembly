631f83f754b89425125f5784c469cd0350f1822f
Grecia Cortes <47579115+Nyoseka@users.noreply.github.com>
2021-12-10 10:50:09 -0800

    Update p3_2.s

--- PracticasC3/P3_2/p3_2.s
+++ PracticasC3/P3_2/p3_2.s
@@ -1,3 +1,8 @@
+@ Practicas Capitulo 2
+@ Archivo p3_2.s
+@ Nombre: Grecia Cortes Valenciano
+@ Fecha: 7 diciembre 2021
+
 .data
 var1 : .asciz " %d\ n"
 seed : .word 1
@@ -12,3 +17,34 @@ main : push { r4, r5 }
 
 @ Llamada a mysrand
 mov r0, # 42
+bl mysrand
+
+@ Inicializar contadir en r4
+mov r4, # 5
+
+@ Bucle que imprime 5 números aleatorios
+bucle : bl myrand @ Leer número aleatorio
+mov r1, r0 @ Valor a r1
+ldr r0, = var1 @ pongo cadena en r0
+bl printf @ llamo a funci ón printf
+subs r4, r4, #1 @ decremento contador
+bne bucle @ salgo si llego a cero
+
+@ Recuperar registros y salir
+pop { r4, r5 }
+bx lr
+
+myrand : ldr r1, = seed
+ldr r0, [ r1 ]
+ldr r2, [ r1, #4 ]
+mul r3, r0, r2
+ldr r0, [ r1, #8 ]
+add r0, r0, r3
+str r0, [ r1 ]
+mov r0, r0, LSL #1
+mov r0, r0, LSR # 17
+bx lr
+
+mysrand : ldr r1, = seed
+str r0, [ r1 ]
+bx lr
