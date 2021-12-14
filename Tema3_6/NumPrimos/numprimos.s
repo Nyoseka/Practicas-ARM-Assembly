8e7fac56b45f260244e0ed2570094966b92ba47e
Grecia Cortes <47579115+Nyoseka@users.noreply.github.com>
2021-12-14 15:07:28 -0800

    Update numprimos.s

--- Tema3_6/NumPrimos/numprimos.s
+++ Tema3_6/NumPrimos/numprimos.s
@@ -20,80 +20,134 @@ _start:
 
 P1:
 ldr r1, =prime		
-mov r0, $2		@ First prime is 2
-str r0, [r1]		@ Store this number in "prime"
+mov r0, $2		@ Toma al 2 como primer numero primo
+str r0, [r1]		
 ldr r0, =limit
-ldr r9, [r0]		@ r2 holds number of primes to find
-mov r3, $3		@ initialize N to 3
-mov r4, $1		@ initialize J to 1
+ldr r9, [r0]		
+mov r3, $3		
+mov r4, $1		
 
-P2:			@ we come here when we have found a prime
-add r4, r4, $1		@ increment J
-str r3, [r1, #4]!	@ store N in "prime" (with writeback)
+P2:		
+add r4, r4, $1		
+str r3, [r1, #4]!	
 
 P3:
-cmp r4, r9		@ check if we are done
-bge P9			@ if so branch to p9
+cmp r4, r9		
+bge P9			
 
 P4:
-add r3, r3, $2		@ add 2 to N 
+add r3, r3, $2		
 
-P5:			@ start checking divisors
-ldr r6, =prime		@ copy pointer to start of "prime"
-ldr r5, [r6]		@ load first divisor
-mov r7, r3		@ copy N into R
-mov r8, $0		@ initialize Q
+P5:			
+ldr r6, =prime		
+ldr r5, [r6]	@ Carga el primer divisor	
+mov r7, r3		
+mov r8, $0		@ Inicializa Q
 
 P6:
-cmp r7, r5		@ if R >= divisor
-subge r7, r7, r5 	@ subtract divisor from R
-addge r8, r8, $1	@ increment Q
-bge P6			@ repeat
-cmp r7, $0		@ if R == 0 ...
-beq P4			@ N is not prime so try next N
+cmp r7, r5		
+subge r7, r7, r5 	
+addge r8, r8, $1	@ Incrementa Q
+bge P6			@ Repite el proceso
+cmp r7, $0		
+beq P4			@ Si el número no es primo, va a P4
 
 P7:
-cmp r8, r5		@ compare Q with divisor
-ble P2			@ if Q <= divisor, N is prime
+cmp r8, r5		
+ble P2			
 
 P8:
-mov r7, r3		@ reset r7 to N
-mov r8, $0		@ reset Q
-ldr r5, [r6, #4]!	@ get next divisor
-bal P6			@ divide again
+mov r7, r3		
+mov r8, $0		
+ldr r5, [r6, #4]!	
+bal P6			
 
 P9:
-mov r0, $1		@ choose stdout
-mov r4, $0		@ use r4 as a temporary counter
-mov r5, $0		@ r5 counts total primes printed
-ldr r6, =prime		@ pointer to "prime"
-ldr r3, [r6]		@ load first prime
+mov r0, $1		
+mov r4, $0		
+mov r5, $0		
+ldr r6, =prime	
+ldr r3, [r6]		
+
+@ Metodo para imprimir
 
 printLoop:
-bl print_num		@ function call
-add r4, $1		@ add one to temp counter
-add r5, $1		@ add one to counter
-cmp r5, r9 		@ are we done?
-bge exit		@ if so, exit
-cmp r4, $9		@ after 10 primes ...
-bgt newline		@ print a newline
-ble space		@ add spaces
+bl print_num		
+add r4, $1		
+add r5, $1		
+cmp r5, r9 		
+bge exit		
+cmp r4, $9		
+bgt newline		
+ble space		
 
-space:                  @ we jump here if
-mov r0, $1              @ we are going to 
-ldr r1, =spc		@ print spaces
+@ Metodo para espacios
+space:                 
+mov r0, $1            
+ldr r1, =spc		
 ldr r2, =len	
 mov r7, $4
 svc $0
-ldr r3, [r6, #4]!	@ load next prime
-bal printLoop           @ continue printing
+ldr r3, [r6, #4]!	
+bal printLoop           
 
-newline:                @ we jump here if
-mov r0, $1              @ we are going to
-ldr r1, =nl             @ print a newline
+@ Metodo saltos de linea
+
+newline:               
+mov r0, $1             
+ldr r1, =nl             
 mov r2, $1
 mov r7, $4
 svc $0
-ldr r3, [r6, #4]!       @ load next prime
-mov r4, $0              @ reset temporary counter
-bal printLoop		@ continue printing
+ldr r3, [r6, #4]!       
+mov r4, $0            
+bal printLoop		@
+
+@ Metodo para imprimir
+
+print_num:
+	stmfd sp!, {r0-r9, lr}	
+	mov r4, $0 		
+	mov r5, $1		
+
+loop:			
+	cmp r3, $9		
+	ble stackPush		
+	sub r3, r3, $10		
+	add r4, r4, $1		
+	bal loop		
+
+stackPush:
+	add r5, r5, $1		
+	orr r0, r3, $0x30	
+	stmfd	sp!, {r0}	
+	cmp r4, $0		
+	beq printChars		
+	mov r3, r4		
+	mov r4, $0		
+	bal loop		
+
+printChars:
+	mov r1, sp		
+	mov r0, $1		
+	mov r2, $1		
+	mov r7, $4		
+	svc $0			
+	subs r5, r5, $1		
+	ble return		
+	ldmfd sp!, {r0}		 
+	bal printChars		
+return:
+	ldmfd sp!, {r0-r9, pc}	
+
+@ Metodo salida
+exit:
+mov r0, $1			
+ldr r1, =nl
+mov r2, $1
+mov r7, $4
+svc $0
+mov r7, $1			
+svc $0
+
+.end
