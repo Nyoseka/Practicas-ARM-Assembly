203a3f4dcf95f42e68ba0d421ae0d3ec36dbe717
Grecia Cortes <47579115+Nyoseka@users.noreply.github.com>
2021-12-14 10:22:33 -0800

    Update p4.s

--- PracticaC4/p4.s
+++ PracticaC4/p4.s
@@ -3,17 +3,20 @@
 @ Nombre: Grecia Cortes Valenciano
 @ Fecha: 13 de diciembre 2021
 
+@ Programa que imprime Hola Mundo, único ejercicio básico del cuadernillo
 .data
 cadena : .asciz " Hola Mundo !\ n "
 cadenafin :
+
 .text
 .global main
-main : push { r7, lr } /* preservamos reg. */
-mov r0, # 1 /* salida est á ndar */
-ldr r1, = cadena /* cadena a enviar */
-mov r2, # cadenafin - cadena /* longitud */
-mov r7, # 4 /* seleccionamos la */
-swi #0 /* llamada a sistema ’write ’*/
-mov r0, # 0 /* devolvemos ok */
-pop { r7, lr } /* recuperamos reg. */
-bx lr
+
+main : push { r7, lr }
+mov r0, # 1 @ Salida
+ldr r1, = cadena @ Enviamos cadena
+mov r2, # cadenafin - cadena 
+mov r7, # 4 
+swi #0 @ Llamada a función write
+mov r0, # 0 
+pop { r7, lr } @ Recuperar registros
+bx lr @ Salida de main
