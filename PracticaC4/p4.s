fec1497f9ab5bc2ff328bf284c0a8124fd07fb16
Grecia Cortes <47579115+Nyoseka@users.noreply.github.com>
2021-12-14 10:18:54 -0800

    Update p4.s

--- PracticaC4/p4.s
+++ PracticaC4/p4.s
@@ -2,3 +2,18 @@
 @ Archivo: p4.s
 @ Nombre: Grecia Cortes Valenciano
 @ Fecha: 13 de diciembre 2021
+
+.data
+cadena : .asciz " Hola Mundo !\ n "
+cadenafin :
+.text
+.global main
+main : push { r7, lr } /* preservamos reg. */
+mov r0, # 1 /* salida est á ndar */
+ldr r1, = cadena /* cadena a enviar */
+mov r2, # cadenafin - cadena /* longitud */
+mov r7, # 4 /* seleccionamos la */
+swi #0 /* llamada a sistema ’write ’*/
+mov r0, # 0 /* devolvemos ok */
+pop { r7, lr } /* recuperamos reg. */
+bx lr
